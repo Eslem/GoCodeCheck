a6f7248fcaa1d24c8640d15514d1e7e1cf2d1f8c
Eslem <j.eslem03@gmail.com>
2019-04-20 19:06:40 +0200

    basic init

---
+++ kata_02.go
@@ -0,0 +1,25 @@
+package kata
+
+func binarySearch(arr []int, key int, start int, end int) int {
+	if start > end {
+		return -1
+	}
+	middle := start + (end-start)/2
+	val := arr[middle]
+
+	if val < key {
+		return binarySearch(arr, key, middle+1, end)
+	} else if val > key {
+		return binarySearch(arr, key, start, end-1)
+	} else {
+		return middle
+	}
+
+}
+
+func chop(key int, arr []int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+	return binarySearch(arr, key, 0, len(arr)-1)
+}
---
+++ kata_02_test.go
@@ -0,0 +1,21 @@
+package kata
+
+import (
+	"testing"
+)
+
+func TestKarateChop(t *testing.T) {
+	arr1 := []int{0, 1, 2, 3, 4, 5, 6}
+
+	sol1 := chop(2, arr1)
+	sol2 := chop(100, arr1)
+
+	if sol1 != 2 {
+		t.Errorf("Error search value %d, result %d", 2, sol1)
+	}
+
+	if sol2 != -1 {
+		t.Errorf("Error search value %d, result %d", 2, sol2)
+	}
+
+}
