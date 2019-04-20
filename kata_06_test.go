f38aba2d8be5a81c378ca68f723f22e47c0a36f8
Eslem <j.eslem03@gmail.com>
2019-04-20 21:24:25 +0200

    public function CalculateAnagram

--- kata_06_test.go
+++ kata_06_test.go
@@ -6,35 +6,35 @@ import (
 
 func BenchmarkCalculateAnagramSingle(b *testing.B) {
 	b.ReportAllocs()
-	calculateAnagram("data/single.txt")
+	CalculateAnagram("data/single.txt")
 }
 
 func BenchmarkCalculateAnagramSmall(b *testing.B) {
 	b.ReportAllocs()
-	calculateAnagram("data/small.txt")
+	CalculateAnagram("data/small.txt")
 }
 
 func BenchmarkCalculateAnagramList(b *testing.B) {
 	b.ReportAllocs()
-	calculateAnagram("data/wordlist.txt")
+	CalculateAnagram("data/wordlist.txt")
 }
 
 func TestCalculateAnagramSingle(t *testing.T) {
-	count, largest := calculateAnagram("data/single.txt")
+	count, largest := CalculateAnagram("data/single.txt")
 	if count != 1 {
 		t.Errorf("Error anagram single %d, result %v", count, largest)
 	}
 }
 
 func TestCalculateAnagramSmall(t *testing.T) {
-	count, largest := calculateAnagram("data/small.txt")
+	count, largest := CalculateAnagram("data/small.txt")
 	if count != 7 {
 		t.Errorf("Error anagram small %d, result %v", count, largest)
 	}
 }
 
 func TestCalculateAnagramList(t *testing.T) {
-	count, largest := calculateAnagram("data/wordlist.txt")
+	count, largest := CalculateAnagram("data/wordlist.txt")
 	if largest[0] != "alerts" {
 		t.Errorf("Error anagram word list count:%d, result %v", count, largest)
 	}
